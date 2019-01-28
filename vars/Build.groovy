ad056f9c0867a3a3ba38fa4181da3d772654f850
davidakr <30952483+davidakr@users.noreply.github.com>
2019-01-28 10:25:46 +0100

    Update and rename log.groovy to Build.groovy

---
+++ vars/Build.groovy
@@ -0,0 +1,76 @@
+def call(body) {
+	node {        
+		agent none
+
+		options {
+			timeout(time: 30, unit: 'MINUTES')
+		}
+		stages {
+			stage('Build_Master') {
+				agent {
+					docker {
+						image 'custom_maven:latest'
+						args '-v /media/data/empty_maven_folder/:/root/.m2:ro -v /media/data/m2-cache/:/home/jenkinsbuild/tmp_cache -m 4G --storage-opt size=20G'
+					}
+				}
+				when {
+					expression {
+						if (env.CHANGE_TARGET) {    // check pull request
+							return false
+						}
+						return env.GIT_BRANCH == 'master'
+					}
+				}
+				stages {
+					stage('load_cache') {
+						steps {
+							sh 'printenv'
+							sh 'mkdir /home/jenkinsbuild/.m2/'
+							sh 'cp -r /home/jenkinsbuild/tmp_cache/. /home/jenkinsbuild/.m2/'
+						}
+					}
+					stage('build') {
+						steps {
+							sh 'mvn -B -DskipTests clean package'
+						}
+					}
+					stage('save_cache') {
+						steps {
+							sh 'cp -r /home/jenkinsbuild/.m2/. /home/jenkinsbuild/tmp_cache/'
+						}
+					}
+				}
+			}
+			stage('Build_Slave') {
+				agent {
+					docker {
+						image 'custom_maven:latest'
+						args '-v /media/data/empty_maven_folder/:/root/.m2:ro -v /media/data/m2-cache/:/home/jenkinsbuild/tmp_cache:ro -m 4G --storage-opt size=20G'
+					}
+				}
+				when {
+					expression {
+						if (env.CHANGE_TARGET) {    
+							return true
+						}
+						return !(env.GIT_BRANCH == 'master')
+					}
+				}
+				stages {
+					stage('load_cache') {
+						steps {
+							sh 'printenv'
+							sh 'mkdir /home/jenkinsbuild/.m2/'
+							sh 'cp -r /home/jenkinsbuild/tmp_cache/. /home/jenkinsbuild/.m2/'
+						}
+					}
+					stage('build') {
+						steps {
+							sh 'mvn -B -DskipTests clean package'
+						}
+					}
+				}
+			}
+		}
+	}
+}
