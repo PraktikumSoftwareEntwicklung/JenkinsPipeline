73712fe7d405354de297d27ab78e37237b2d4457
davidakr <30952483+davidakr@users.noreply.github.com>
2019-02-07 15:12:09 +0100

    Update buildPipeline.groovy

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -25,6 +25,7 @@ def call() {
 					stage('load_cache') {
 						steps {
 							sh 'printenv'
+							sh 'echo MASTER_PING'
 							sh 'mkdir /home/jenkinsbuild/.m2/'
 							sh 'cp -r /home/jenkinsbuild/tmp_cache/. /home/jenkinsbuild/.m2/'
 						}
