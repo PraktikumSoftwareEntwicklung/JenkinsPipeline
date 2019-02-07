dcc91a0bac95d72eab9fcac1d26e124e8997f5f5
davidakr <30952483+davidakr@users.noreply.github.com>
2019-02-07 16:10:15 +0100

    Update buildPipeline.groovy

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -72,18 +72,18 @@ def call() {
 				}
 			}
 		}
-	}
-	post {
-		always {
-			dir("${env.WORKSPACE}@tmp") {
-				deleteDir()
-			}
-			dir("${env.WORKSPACE}@script") {
-				deleteDir()
-			}
-			dir("${env.WORKSPACE}@script@tmp") {
-				deleteDir()
+		post {
+			always {
+				dir("${env.WORKSPACE}@tmp") {
+					deleteDir()
+				}
+				dir("${env.WORKSPACE}@script") {
+					deleteDir()
+				}
+				dir("${env.WORKSPACE}@script@tmp") {
+					deleteDir()
+				}
 			}
 		}
-	}
+	}	
 }
