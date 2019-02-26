a3516bc595b6ee5ffb139aa036ca7c5dd139295c
Jannik Prinz <jannik.prinz@gmail.com>
2019-02-26 19:57:45 +0100

    Enable deploy for non release builds on the master branch. Activate JavaDoc and CodeMetric analysis.

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -2,10 +2,10 @@
 
 def call(body) {
 
-	def config = [:]
-	body.resolveStrategy = Closure.DELEGATE_FIRST
-	body.delegate = config
-	body()
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
     
     // TODO: Add project name and branch name
     def MavenContainerName = "MyMavenContainer_" + env.BUILD_ID
@@ -13,31 +13,33 @@ def call(body) {
     def MavenPwd = ""
 
     def tasks = [:]
-	def doRelease = false
+    def doDeploy = false
+    def doRelease = false
+    def releaseVersion = "0.1"
     def doPostProcessing = false
     def postProcessingFinished = false
     
-    tasks["Jenkins_Container"] = {		
+    tasks["Jenkins_Container"] = {
         while (!doPostProcessing) {
             sleep(5)
         }
-		if(doRelease) {
-			deploy(config,BuildFilesFolder, MavenContainerName, MavenPwd)
-		}       
+        if(doDeploy) {
+            postProcessBuildResults(config, BuildFilesFolder, MavenContainerName, MavenPwd, doRelease, releaseVersion)
+        }
         postProcessingFinished = true
     }
-    
+
     tasks["Maven_Container"] = {
         pipeline {
             agent any
-			
-			parameters {
-				booleanParam (name: 'RELEASE', defaultValue: false, description: 'Set true for Release')
-			}
-		
-			environment {
-				GIT_COMMIT_EMAIL = """${sh(returnStdout: true,script: 'git --no-pager show -s --format=\'%ae\'')}""".trim()
-			}
+
+            parameters {
+                booleanParam (name: 'RELEASE', defaultValue: false, description: 'Set true for Release')
+            }
+
+            environment {
+                GIT_COMMIT_EMAIL = """${sh(returnStdout: true,script: 'git --no-pager show -s --format=\'%ae\'')}""".trim()
+            }
 
             options {
                 timeout(time: 30, unit: 'MINUTES')
@@ -73,7 +75,8 @@ def call(body) {
                                         script: 'pwd',
                                         returnStdout: true
                                     ).trim()
-									doRelease = params.RELEASE
+                                    doRelease = params.RELEASE
+                                    doDeploy = true
                                     doPostProcessing = true
                                     while (!postProcessingFinished) {
                                         sleep(5)
@@ -114,9 +117,19 @@ def call(body) {
                         stage('build') {
                             steps {
                                 sh 'mvn clean verify'
+                                script {
+                                    MavenPwd = sh (
+                                        script: 'pwd',
+                                        returnStdout: true
+                                    ).trim()
+                                    doPostProcessing = true
+                                    while (!postProcessingFinished) {
+                                        sleep(5)
+                                    }
+                                }
                             }
                         }
-                    }                 
+                    }
                 }
             }
         }
@@ -125,24 +138,24 @@ def call(body) {
     parallel tasks
 }
 
-def deploy(config, BuildFilesFolder, MavenContainerName, MavenPwd) {
+def postProcessBuildResults(config, BuildFilesFolder, MavenContainerName, MavenPwd, doRelease, releaseVersion) {
     node {
-	def mandatoryParameters = ['sshConfigName', 'absoluteWebserverDir', 'webserverDir', 'updateSiteLocation']
-	for (mandatoryParameter in mandatoryParameters) {
-		if (!config.containsKey(mandatoryParameter) || config.get(mandatoryParameter).toString().trim().isEmpty()) {
-			error "Missing mandatory parameter $mandatoryParameter"
-		}			
-	}
+        def mandatoryParameters = ['sshConfigName', 'absoluteWebserverDir', 'webserverDir', 'updateSiteLocation']
+        for (mandatoryParameter in mandatoryParameters) {
+            if (!config.containsKey(mandatoryParameter) || config.get(mandatoryParameter).toString().trim().isEmpty()) {
+                error "Missing mandatory parameter $mandatoryParameter"
+            }
+        }
 
-	boolean skipCodeQuality = config.containsKey('skipCodeQuality') && config.get('skipCodeQuality').toString().trim().toBoolean()
-	boolean skipNotification = config.containsKey('skipNotification') && config.get('skipNotification').toString().trim().toBoolean()
+        boolean skipCodeQuality = config.containsKey('skipCodeQuality') && config.get('skipCodeQuality').toString().trim().toBoolean()
+        boolean skipNotification = config.containsKey('skipNotification') && config.get('skipNotification').toString().trim().toBoolean()
 
-	sh "echo ${config.sshConfigName}"
-	sh "echo ${config.absoluteWebserverDir}"
-	sh "echo ${config.webserverDir}"
-	sh "echo ${config.updateSiteLocation}"
-		
-		// TODO: move /$updateSiteLocation to 'docker cp'
+        sh "echo ${config.sshConfigName}"
+        sh "echo ${config.absoluteWebserverDir}"
+        sh "echo ${config.webserverDir}"
+        sh "echo ${config.updateSiteLocation}"
+
+        // TODO: move /$updateSiteLocation to 'docker cp'
         String usl = "$BuildFilesFolder/${config.updateSiteLocation}"
 
         sh "echo $usl"
@@ -151,17 +164,13 @@ def deploy(config, BuildFilesFolder, MavenContainerName, MavenPwd) {
         sh "docker cp $MavenContainerName:$MavenPwd $BuildFilesFolder"
 
         try {
+            // deploy:
             sshPublisher(
                 failOnError: true,
                 publishers: [
                     sshPublisherDesc(
                         configName: "${config.sshConfigName}",
                         transfers: [
-                            /*sshTransfer(
-                                execCommand:
-                                "mkdir -p ${config.absoluteWebserverDir}/${config.webserverDir}/nightly &&" +
-                                "rm -rf ${config.absoluteWebserverDir}/${config.webserverDir}/nightly/*"
-                            ),*/
                             sshTransfer(
                                 sourceFiles: "$usl/**/*",
                                 cleanRemote: true,
@@ -172,20 +181,76 @@ def deploy(config, BuildFilesFolder, MavenContainerName, MavenPwd) {
                     )
                 ]
             )
+            if (doReleaseBuild) {
+                sshPublisher(
+                    failOnError: true,
+                    publishers: [
+                        sshPublisherDesc(
+                            configName: "${config.sshConfigName}",
+                            transfers: [
+                                sshTransfer(
+                                    execCommand:
+                                    "rm -rf $absoluteWebserverDir/${config.webserverDir}/releases/latest &&" +
+                                    "rm -rf $absoluteWebserverDir/${config.webserverDir}/releases/$releaseVersion &&" +
+                                    "mkdir -p $absoluteWebserverDir/${config.webserverDir}/releases/$releaseVersion &&" +
+                                    "cp -a $absoluteWebserverDir/${config.webserverDir}/nightly/* $absoluteWebserverDir/releases/$releaseVersion/ &&" +
+                                    "ln -s $absoluteWebserverDir/${config.webserverDir}/releases/$releaseVersion $absoluteWebserverDir/${config.webserverDir}/releases/latest"
+                                )
+                            ]
+                        )
+                    ]
+                )
+            }
+
+            // archive:
+            archiveArtifacts "$usl/**/*"
+
+            boolean skipCodeQuality = config.containsKey('skipCodeQuality') && config.get('skipCodeQuality').toString().trim().toBoolean()
+            if (!skipCodeQuality) {
+                sh "echo 'Publish JavaDoc'"
+                publishHTML([
+                    allowMissing: false,
+                    alwaysLinkToLastBuild: false,
+                    keepAll: false,
+                    reportDir: "$usl/javadoc",
+                    reportFiles: 'overview-summary.html',
+                    reportName: 'JavaDoc',
+                    reportTitles: ''
+                ])
+
+                checkstyle([
+                    pattern: '**/target/checkstyle-result.xml'
+                ])
+                junit([
+                    testResults: '**/surefire-reports/*.xml',
+                    allowEmptyResults: true
+                ])
+                jacoco([
+                    execPattern: '**/target/*.exec',
+                    classPattern: '**/target/classes',
+                    sourcePattern: '**/src,**/src-gen,**/xtend-gen',
+                    inclusionPattern: '**/*.class',
+                    exclusionPattern: '**/*Test*.class'
+                ])
+            }
+            else {
+                sh "echo 'Skip JavaDoc and CodeQuality'"
+            }
+
         } catch (err) {
             sh "echo 'An error occured!'"
             String errMsg = err.getMessage()
             sh "echo $errMsg"
-            /*currentBuild.result = 'FAILURE'
+            currentBuild.result = 'FAILURE'
             if (err instanceof hudson.AbortException && err.getMessage().contains('script returned exit code 143')) {
                 currentBuild.result = 'ABORTED'
             }
             if (err instanceof org.jenkinsci.plugins.workflow.steps.FlowInterruptedException && err.causes.size() == 0) {
                 currentBuild.result = 'ABORTED'
             }
-            throw err*/
+            throw err
         }
-        
+
         sh "rm -rf $BuildFilesFolder"
     }
 }
